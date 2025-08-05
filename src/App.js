d0074b3b1b819176f7d2f9fcef51632cbbd65e37
Nareman Abdalla <120642444+naremanabdalla@users.noreply.github.com>
2025-08-05 11:15:00 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Webchat from '@botpress/webchat';
+import { Webchat } from '@botpress/webchat'; // <-- use named import
 
 function App() {
   return (
